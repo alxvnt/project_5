fe8620531e978496f27eb4be8a03831e4ed71975
alxvnt <alixvoinot1@gmail.com>
2018-03-27 11:15:17 +0200

    add file db_creator.py

---
+++ db_creator.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+
+"""
+This script collect the data from the Openfoodfacts file in CSV format.
+
+Then, the script will create the database of the application and will
+insert the data collected into the table.
+
+Author: Alix VOINOT
+
+"""
+
+import mysql.connector
+
+def csv_reader():
+    pass
+
+def create_db():
+    pass
+    
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
